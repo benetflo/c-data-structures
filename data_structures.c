286d7fd648bd1ca13b36e9e38e88cfba54753019
benflo <benji@DESKTOP-JQVMOHH.localdomain>
2025-08-19 10:43:16 +0200

    added slinkedlist implementation

--- data_structures.c
+++ data_structures.c
@@ -63,18 +63,78 @@ void pop_back(Vector *vec){
 	vec->elements--;
 }
 
+/*
+	SINGLE LINKED LIST IMPLEMENTATION
+
+*/
+
+typedef struct Node{
+	int data;
+	struct Node* next;
+}Node;
+
+Node* create_slinked_list(){
+
+	Node* head = NULL;
+	return head;
+}
+
+void add_back(Node** head, int data){
+
+	Node* n = malloc(sizeof(Node));
+	n->data = data;
+
+	// if list is empty
+	if(*head == NULL){
+		*head = n;
+	}else{
+		Node* current = *head;
+		while(current->next != NULL){
+			current = current->next;
+		}
+		current->next = n;
+	}
+	n->next = NULL;
+}
+
+void add_front(Node** head, int data){
+
+	Node* n = malloc(sizeof(Node));
+	n->data = data;
+
+	n->next = *head;
+	*head = n;
+}
+
+void remove_node_byindex(Node** head, uint8_t index){
+
+	if(*head == NULL) return;
+
+	if(index == 0){
+		Node* temp = *head;
+		*head = (*head)->next;
+		free(temp);
+		return;
+	}
+
+	Node* current = *head;
+
+	for(int i = 0; i < index - 1; i++){
+		current = current->next;
+	}
+
+	Node* temp = current->next;
+
+	if (temp == NULL){
+		 return;
+	}
+
+	current->next = temp->next;
+	free(temp);
+}
+
 int main(){
 
-	Vector v = create_vector(3);
-	add_back(&v, 12);
-	add_back(&v, 15);
-	add_back(&v, 0);
-	add_back(&v, 1);
-
-	pop_back(&v);
-
-	for(int i = 0; i < v.elements; i++){
-		printf("%d\n", v.array[i]);
-	}
-	return 0;
+	Node* list = create_slinked_list();
+	add_back(&list, 10);
 }
