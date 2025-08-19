b876f047669388a8c799ce56e20c98fd343ceee8
benflo <benji@DESKTOP-JQVMOHH.localdomain>
2025-08-19 12:01:53 +0200

    added slink list functions

--- data_structures.c
+++ data_structures.c
@@ -108,8 +108,9 @@ void add_front(Node** head, int data){
 
 void remove_node_byindex(Node** head, uint8_t index){
 
-	if(*head == NULL) return;
-
+	if(*head == NULL){
+		return;
+	}
 	if(index == 0){
 		Node* temp = *head;
 		*head = (*head)->next;
@@ -131,6 +132,60 @@ void remove_node_byindex(Node** head, uint8_t index){
 
 	current->next = temp->next;
 	free(temp);
+}
+
+void remove_node_byvalue(Node** head, int data){
+
+	if(*head == NULL){
+		return;
+	}
+
+	if((*head)->data == data){
+		Node* temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}else{
+		Node* current = *head;
+
+		while(current->next != NULL && (current->next)->data != data){
+			current = current->next;
+		}
+		if(current->next != NULL){
+
+			Node* temp = current->next;
+			current->next = temp->next;
+			free(temp);
+		}
+	}
+}
+
+void insert_at_index(Node** head, uint8_t index, int data){
+
+	Node* n = malloc(sizeof(Node));
+	n->data = data;
+
+	if(index == 0){
+		add_front(head, data);
+		return;
+	}
+
+	Node* current = *head;
+
+	for(int i = 0; i < index - 1; i++){
+
+		if(current == NULL){
+			return;
+		}
+		current = current->next;
+	}
+
+	if(current == NULL){ // if list is too short
+		return;
+	}
+
+	Node* temp = current->next; // the actual index;
+	current->next = n;
+	n->next = temp;
 }
 
 int main(){
